1252196b860b6f806e6bae31bcb2516aef52515b
Caine <143353083+Caine-Spiwak@users.noreply.github.com>
2023-12-09 10:34:06 -0800

    render the todos for each task

--- src/js/renderTodos.js
+++ src/js/renderTodos.js
@@ -3,23 +3,26 @@ import projects from "./projects";
 const renderTodos = (index) => {
 
     // GET TODOS FOR ACTIVE PROJECTS TASKS
-    const tasks = projects[index].tasks
+    const tasks = projects[index].tasks     //[0].todos
 
     // RENDER TODOS
     tasks.forEach((task, i) => {
         const currentTask = document.querySelector(`[data-task-index='${i}']`)
+        const currentTodos = tasks[i].todos
         const todoList = document.createElement('ol')
+        todoList.classList.add('todo-list')
 
-        todoList.innerHTML += `
+        currentTodos.forEach((todo, i) => {
+            
+            todoList.innerHTML += `
             <li data-todo-index='${i}' class="todo">
-                <span class="todo-description">${task.todos.description}</span>
+                <span class="todo-description">${todo.description}</span>
             </li>
-        `;
+            `;
 
-        currentTask.appendChild(todoList)
+            currentTask.appendChild(todoList)
+        })
     })
-
-    
 }
 
 export default renderTodos;
